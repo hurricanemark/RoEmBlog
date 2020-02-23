a9b67f0ce2cc7ce0c4ac8ec406e6bfc160413667
Mark Nguyen <hurricanemark@gmail.com>
2020-02-23 00:48:18 -0800

    add User document to mongodb

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//const mongoose = require('mongoose')
+//const Schema = mongoose.Schema;
+
 const BlogPostSchema = new Schema({ 
     title: String, 
     body: String,
